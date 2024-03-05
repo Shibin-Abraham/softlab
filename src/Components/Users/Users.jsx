678eca9ba19406db550fd1bd4313cc007a04d411
SHIBIN K A <shibin.k.a6235@gmail.com>
2024-03-05 12:32:12 +0530

    users component style changed

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -8,57 +8,60 @@ function Users() {
   return (
     <div className='users'>
       <section>
-        <h1>Users</h1>
-        <div className='container'>
-            <div className='table-section'>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Phone</th>
-                            <th>Role</th>
-                            <th>Join Date</th>
-                            <th>Status</th>
-                            <th>Operation</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td> soft lab</td>
-                            <td> softlab@gmail.com</td>
-                            <td> 1913022024</td>
-                            <td> Lab Manager</td>
-                            <td> 20-02-24</td>
-                            {<Action/>}
-                        </tr>
-                        <tr>
-                            <td> soft lab</td>
-                            <td> softlab@gmail.com</td>
-                            <td> 1913022024</td>
-                            <td> Lab Assistant</td>
-                            <td> 20-02-24</td>
-                            {<Action/>}
-                        </tr>
-                        <tr>
-                            <td> soft lab</td>
-                            <td> softlab@gmail.com</td>
-                            <td> 1913022024</td>
-                            <td> Lab Assistant</td>
-                            <td> 20-02-24</td>
-                            {<Action/>}
-                        </tr>
-                        <tr>
-                            <td> soft lab</td>
-                            <td> softlab@gmail.com</td>
-                            <td> 1913022024</td>
-                            <td>  Lab Assistant</td>
-                            <td> 20-02-24</td>
-                            {<Action/>}
-                        </tr>
-                        
-                    </tbody>
-                </table>
+        <div className="top-container">
+            <h1>Users</h1>
+            <button>New Role</button>
+        </div>
+        <div className="mid-box">
+            <div className='container'>
+                <div className='table-section'>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Role</th>
+                                <th>Join Date</th>
+                                <th>Status</th>
+                                <th>Operation</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td> soft lab</td>
+                                <td> softlab@gmail.com</td>
+                                <td> Lab Manager</td>
+                                <td> 20-02-24</td>
+                                {<Action/>}
+                            </tr>
+                            <tr>
+                                <td> soft lab</td>
+                                <td> softlab@gmail.com</td>
+                                <td> Lab Assistant</td>
+                                <td> 20-02-24</td>
+                                {<Action/>}
+                            </tr>
+                            <tr>
+                                <td> soft lab</td>
+                                <td> softlab@gmail.com</td>
+                                <td> Lab Assistant</td>
+                                <td> 20-02-24</td>
+                                {<Action/>}
+                            </tr>
+                            <tr>
+                                <td> soft lab</td>
+                                <td> softlab@gmail.com</td>
+                                <td>  Lab Assistant</td>
+                                <td> 20-02-24</td>
+                                {<Action/>}
+                            </tr>
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div className="container-2">
+
             </div>
         </div>
       </section>
