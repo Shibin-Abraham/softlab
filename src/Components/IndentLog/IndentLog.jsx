0bc65ae7a6dcbffb2660e50619723c0a12fedc31
SHIBIN K A <shibin.k.a6235@gmail.com>
2024-04-28 16:53:21 +0530

    stock filter partial

--- src/Components/IndentLog/IndentLog.jsx
+++ src/Components/IndentLog/IndentLog.jsx
@@ -79,11 +79,11 @@ function IndentLog() {
             <div className="top">
                 <h1>Indent Log</h1> 
                 <div onClick={()=>setAddIndentPopUp(true)}> 
-                <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path d="m17.5 11c2.484 0 4.5 2.016 4.5 4.5s-2.016 4.5-4.5 4.5-4.5-2.016-4.5-4.5 2.016-4.5 4.5-4.5zm.5 4v-1.5c0-.265-.235-.5-.5-.5s-.5.235-.5.5v1.5h-1.5c-.265 0-.5.235-.5.5s.235.5.5.5h1.5v1.5c0
                      .265.235.5.5.5s.5-.235.5-.5c0-.592 0-1.5 0-1.5h1.5c.265 0 .5-.235.5-.5s-.235-.5-.5-.5c-.592 0-1.5 0-1.5 0zm-6.479 1c.043.522.153 1.025.321 1.5h-9.092c-.414 0-.75-.336-.75-.75s.336-.75.75-.75zm1.106-4c-.328.456-.594.96-.785
                       1.5h-9.092c-.414 0-.75-.336-.75-.75s.336-.75.75-.75zm7.373-3.25c0-.414-.336-.75-.75-.75h-16.5c-.414 0-.75.336-.75.75s.336.75.75.75h16.5c.414 0 .75-.336.75-.75zm0-4c0-.414-.336-.75-.75-.75h-16.5c-.414 0-.75.336-.75.75s.336.75.75.75h16.5c.414 0
-                       .75-.336.75-.75z" fill-rule="nonzero"/>
+                       .75-.336.75-.75z" fillRule="nonzero"/>
                 </svg>
             </div>
             </div>
@@ -116,7 +116,7 @@ function IndentLog() {
                 
             </div>
         </section> 
-        {addIndentPopUp && <AddIndentPopUp setAddIndentPopUp={setAddIndentPopUp} allStockData={allStockData}/>}
+        {addIndentPopUp && <AddIndentPopUp setAddIndentPopUp={setAddIndentPopUp} allStockData={allStockData} allIndentData={allIndentData} />}
     </div>
   )
 }
--- src/Components/IndentLog/PopUp/AddIndentPopUp.jsx
+++ src/Components/IndentLog/PopUp/AddIndentPopUp.jsx
@@ -1,12 +1,39 @@
 import React, { useEffect, useState } from 'react'
 import './AddIndentPopUp.css'
+import { useForm } from 'react-hook-form';
 
 function AddIndentPopUp(props) {
+    console.log(props)
     let [popUpActive,setPopUpActive] = useState('')
+    let [filteredStock,setFilteredStock] = useState([])
+    let [error,setError] = useState('')
+
+    const {register, handleSubmit ,formState:{errors}} = useForm();
+
+    let onSubmit = (data)=>{
+        if(data && error.length===0){
+            console.log(data)
+        }
+    }
 
     useEffect(()=>{
+        props.allStockData.forEach((d)=>{
+            console.log('sir',d.id)
+            for(let i=0;i<props.allIndentData.length;i++){
+                if(props.allIndentData[i].stock_id !== d.id){
+                    let obj = [id: d.id,name: d.name]
+                    setFilteredStock((prev)=>[prev,...obj])
+                    console.log("**",filteredStock)
+                    break
+                }
+            }
+            
+        })
+        //setFilteredStock(props.allStockData.filter((data,index)=>index!==props.allIndentData.lenght?props.allIndentData[index].stock_id!==data.id:null
+        //))
+
         setTimeout(()=>setPopUpActive('popup-active'),1)
-    },[])
+    },[props.allIndentData,props.allStockData,filteredStock])
   return (
     <div className='add-indent-popup'>
             <div className={`content ${popUpActive}`}>
@@ -18,22 +45,38 @@ function AddIndentPopUp(props) {
                         props.setAddIndentPopUp(false)
                     },300)
                 }}>
-                <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z" clipRule="evenodd" />
+                <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12
+                 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06
+                  12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z" clipRule="evenodd" />
                 </svg>
                 </div>
-                <div className="data-section">
-                <div>
-                <select >
-                    {
-                                <option value="">select</option>
-                    }
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="data-section">
+                    <div>
+                    <select {...register("stockid",{required:true})}>
+                        {
+                               filteredStock.map((data,index)=>{
+                                return(
+                                    <option key={index} value={data.id}>{data.name}</option>
+                                )
+                               })   
+                        }
+                        
+                    </select>
+                    <p>
+                        {errors.stockid?.type ==="required"&& "Please select one option"}
+                        
+                    </p>
+                    </div>
                     
-                </select>
-                </div>
-                <p></p>
-                <input type="file" id="images" accept="image/*" required></input>
-                <input type="button" value="ADD"  />
-                </div>
+                    <input type="file" accept="image/png,image/jpeg" {...register("img",{required:true})}/>
+                    <p>
+                        {errors.img?.type ==="required"&& "Please upload one img"}
+                    </p>
+                    <input type="submit" value="Submit"  />
+                    </div>
+                </form>
+                
             </div>
       {/*<h2 onClick={()=>props.setPopUpActive(false)}>POPup</h2>*/}
     </div>
