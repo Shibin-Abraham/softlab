058dd670f7b26908bf55dce47200c17cadbcdb9c
SHIBIN K A <shibin.k.a6235@gmail.com>
2024-07-15 23:31:01 +0530

    update : login api changed

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -17,37 +17,37 @@ function Login({ setNav }) {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const navigate = useNavigate()
 
-  useEffect(() => {
-    axios({
-      method: 'POST',
-      url: 'http://localhost/soft-lab-api/route/middleware/remember-me.php',
-      data: { JWT: localStorage.getItem('token') !== null ? localStorage.getItem('token') : '' }
-    }).then((res) => {
-      if (res.data.statuscode === 200) {
-        if (res.data.status === '0') {  // check wheather account is active or not 
-          navigate('/inactive', { replace: true })
-        } else {
-          dispatch({
-            type: 'auth_login',
-            JWT: localStorage.getItem('token') !== null ? localStorage.getItem('token') : '',
-            u_id: res.data.u_id,
-            name: res.data.name,
-            email: res.data.email,
-            phone: res.data.phone,
-            join_date: res.data.join_date,
-            status: res.data.status,
-            r_id: res.data.r_id,
-            r_name: res.data.r_name
-          })
-          console.log("****", res)
-          setNav(res.data.r_id)
-          navigate('/', { replace: true })
-        }
-      }
-    }).catch((err) => {
-      navigate('/login', { replace: true })
-    })
-  }, [dispatch, setNav, navigate])
+  // useEffect(() => {
+  //   axios({
+  //     method: 'POST',
+  //     url: 'http://localhost/soft-lab-api/route/middleware/remember-me.php',
+  //     data: { JWT: localStorage.getItem('token') !== null ? localStorage.getItem('token') : '' }
+  //   }).then((res) => {
+  //     if (res.data.statuscode === 200) {
+  //       if (res.data.status === '0') {  // check wheather account is active or not 
+  //         navigate('/inactive', { replace: true })
+  //       } else {
+  //         dispatch({
+  //           type: 'auth_login',
+  //           JWT: localStorage.getItem('token') !== null ? localStorage.getItem('token') : '',
+  //           u_id: res.data.u_id,
+  //           name: res.data.name,
+  //           email: res.data.email,
+  //           phone: res.data.phone,
+  //           join_date: res.data.join_date,
+  //           status: res.data.status,
+  //           r_id: res.data.r_id,
+  //           r_name: res.data.r_name
+  //         })
+  //         console.log("****", res)
+  //         setNav(res.data.r_id)
+  //         navigate('/', { replace: true })
+  //       }
+  //     }
+  //   }).catch((err) => {
+  //     navigate('/login', { replace: true })
+  //   })
+  // }, [dispatch, setNav, navigate])
 
   let onSubmit = (data) => {
     if (data) {
@@ -56,7 +56,7 @@ function Login({ setNav }) {
       setLoader(true)
       axios({
         method: 'POST',
-        url: 'http://localhost/soft-lab-api/route/login.php',
+        url: 'http://localhost:4000/login',
         data: data
       }).then((res) => {
         setLoader(false)
@@ -64,42 +64,56 @@ function Login({ setNav }) {
         checkResponse(res)
       }).catch((err) => {
         setLoader(false)
-        setGlobalPopUp({ id: 4, header: `${err.message}!`, message: `${err.message}! please check your network` })
+        errorHandler(err)
+        // setGlobalPopUp({ id: 4, header: '', message: `${err.message}! please check your network` })
       })
     }
   }
-  function checkResponse(res) {
-    if (res.data.statuscode === 200 && res.data.JWT !== null) {
-      //setGlobalPopUp({id:1,header:`Success`,message:`Successfully loged in`})
-      if (res.data.status === '0') {  // check wheather account is active or not 
-        navigate('/inactive', { replace: true })
-      } else {
-        console.log("inside res f()", res.data)
-        localStorage.setItem('token', res.data.JWT)
-        dispatch({
-          type: 'auth_login',
-          JWT: res.data.JWT,
-          u_id: res.data.u_id,
-          name: res.data.name,
-          email: res.data.email,
-          phone: res.data.phone,
-          join_date: res.data.join_date,
-          status: res.data.status,
-          r_id: res.data.r_id,
-          r_name: res.data.r_name
-        })
-        setNav(res.data.r_id)
-        navigate('/', { replace: true })
-      }
-    } else if (res.data.statuscode === 401 && res.data.password) {
-      setErrorMsg('Invalid password')
-      setGlobalPopUp({ id: 4, header: `Invalid input`, message: `Invalid password, Please try again` })
-    } else if (res.data.statuscode === 401) {
-      setErrorMsg('Email not found')
-      setGlobalPopUp({ id: 4, header: `Email not found`, message: `Email not found, Please goto signup page` })
-    } else if (res.data.statuscode === 400) {
-      setGlobalPopUp({ id: 3, header: 'Bad request', message: 'please check your request' })
+  const errorHandler = ({ response }) => {
+    setLoader(false)
+    if (response.status === 403) { setGlobalPopUp({ id: 4, header: 'Error', message: response.data.error }); return }
+    if (response.status === 401) {
+      setGlobalPopUp({ id: 4, header: 'Invalid credentials', message: `${response.data.error}, Please try again` })
+      setErrorMsg(response.data.error)
+      return
     }
+
+    setGlobalPopUp({ id: 4, header: 'Error occured', message: response.data.error })
+  }
+  function checkResponse(res) {
+
+
+    //   if (res.data.statuscode === 200 && res.data.JWT !== null) {
+    //     //setGlobalPopUp({id:1,header:`Success`,message:`Successfully loged in`})
+    //     if (res.data.status === '0') {  // check wheather account is active or not 
+    //       navigate('/inactive', { replace: true })
+    //     } else {
+    //       console.log("inside res f()", res.data)
+    //       localStorage.setItem('token', res.data.JWT)
+    //       dispatch({
+    //         type: 'auth_login',
+    //         JWT: res.data.JWT,
+    //         u_id: res.data.u_id,
+    //         name: res.data.name,
+    //         email: res.data.email,
+    //         phone: res.data.phone,
+    //         join_date: res.data.join_date,
+    //         status: res.data.status,
+    //         r_id: res.data.r_id,
+    //         r_name: res.data.r_name
+    //       })
+    //       setNav(res.data.r_id)
+    //       navigate('/', { replace: true })
+    //     }
+    //   } else if (res.data.statuscode === 401 && res.data.password) {
+    //     setErrorMsg('Invalid password')
+    //     setGlobalPopUp({ id: 4, header: `Invalid input`, message: `Invalid password, Please try again` })
+    //   } else if (res.data.statuscode === 401) {
+    //     setErrorMsg('Email not found')
+    //     setGlobalPopUp({ id: 4, header: `Email not found`, message: `Email not found, Please goto signup page` })
+    //   } else if (res.data.statuscode === 400) {
+    //     setGlobalPopUp({ id: 3, header: 'Bad request', message: 'please check your request' })
+    //   }
 
   }
   return (
